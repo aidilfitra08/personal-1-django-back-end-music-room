f2fdc9adb66115237601f47490aa01c81ad6ec4d
aidilfitra08 <aidil.gitu1@gmail.com>
2025-02-22 17:11:29 +0700

    add new create end point

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.main),
-    path('room', views.RoomView.as_view())
+    path('room', views.RoomView.as_view()),
+    path('create-room', views.CreateRoomView.as_view())
 ]
